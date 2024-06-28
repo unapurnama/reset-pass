5800afe3a7740a9d5bc46fd86a64a0b2a003ab26
unapurnama <unnaningsi@gmail.com>
2024-06-28 16:39:20 +0800

    Update proses_reset.php

--- proses_reset.php
+++ proses_reset.php
@@ -1,6 +1,6 @@
 <?php
-
 include "koneksi.php";
+$email      = $_POST['username'];
 $cek =  mysqli_num_rows(mysqli_query($koneksi, "SELECT * FROM tbl_users 
 WHERE email='$email'"));
 if ($cek > 0) {
